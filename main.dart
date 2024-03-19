8d825e849ca8e55ba5f011690b7d63dce86a062e
Yeswadams <yeswaadams73@gmail.com>
2024-03-19 18:29:33 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+int add(int a, int b) {
+  int sum = a + b;
+  return sum;
+}
+
+void MAIN() {
+  int num1 = 10;
+  int num2 = 20;
+  int total = add(num1, num2);
+  print("The sum of $num1 and $num2 is $total");
+}
